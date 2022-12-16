002d3a60d5ff2891a67f22be2d28af15e85de4df
Sandro Dias <devwiise@gmail.com>
2022-12-16 16:42:19 -0300

    Função do darkmode adicionada

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
 
 
-function calc() {
+function calcIMC() {
    // const checkMen = document.getElementById('men').checked
-   const inptWeight = document.getElementById('weight')
-   const inptHeight = document.getElementById('height')
-   const outMsg = document.getElementById('outMsg').innerText = 'seu IMC é:'
+   const inWeight = document.getElementById('weight')
+   const inHeight = document.getElementById('height')
+   document.getElementById('outMsg').innerText = 'seu IMC é:'
    const outIMC = document.getElementById('yourImc')
    // convert
-   const peso = Number(inptWeight.value)
-   const altura = Number(inptHeight.value)
+   const peso = Number(inWeight.value)
+   const altura = Number(inHeight.value)
    
    const imc = (peso / (altura * altura)) 
-   
-   if((peso === 0 || peso === isNaN(peso)) && (altura === 0 || altura === isNaN(altura)) ) {
-      alert('Preenchar todos os campos') 
-      return 
-   }
-   
+   console.log(typeof inWeight)
+
    
    return outIMC.innerText = `${imc.toFixed(2)}`
 }
 
+
+// changeBodyColor 
+function darkMode() {
+   const inDarkMode = document.getElementById('darkMode')   
+   inDarkMode.classList.toggle('dkMode')
+}
 
 // const btnCalc = document.getElementById('buttonCalc')
 // btnCalc.addEventListener('click', calc())
