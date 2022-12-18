3674fce4661ae912949e9e8a342ebaf3faac173c
Sandro Dias <devwiise@gmail.com>
2022-12-18 02:50:27 -0300

    DarkMode melhorado

--- js/main.js
+++ js/main.js
@@ -13,32 +13,32 @@ function calcIMC() {
    const imc = (peso / (altura * altura)) 
    console.log(typeof inWeight)
 
-   
    return outIMC.innerText = `${imc.toFixed(2)}`
 }
 
 
 function darkMode() {
    const changeColorBody = document.getElementById('darkMode')   
-   const changeColorTitleIMC = document.getElementById('imcTitle')
-   const changeColorParagraphIMC = document.getElementById('imcParagraph')
-   const changeColorGenrerTitle = document.getElementById('genreTitle')
-   const changeColorGenrerParagraph = document.getElementById('genreParagraph')
-   const changeColorLabelMen = document.getElementById('menLabel')
-   const changeColorLabelWoman = document.getElementById('womanLabel')
-   const changeColorInputTitle = document.getElementById('inputTitle')
+   changeColorBody.classList.toggle('containerDarkMode')
 
-
-
-   changeColorBody.classList.toggle('dkMode')
-   changeColorTitleIMC.classList.toggle('titleDark')
-   changeColorGenrerTitle.classList.toggle('titleDark')
-   changeColorParagraphIMC.classList.toggle('paragraphDark')
-   changeColorGenrerParagraph.classList.toggle('titleDark')
-   changeColorLabelMen.classList.toggle('paragraphDark')
-   changeColorLabelWoman.classList.toggle('paragraphDark')
-   changeColorInputTitle.classList.toggle('titleDark')
 }
 
+// Effects input
+const inputLabel = document.querySelectorAll('.input__label')
+const inputImc = document.querySelectorAll('.input__imc')
 
+inputImc.forEach((element, i) => {
+   
+   element.addEventListener('focus', (e) => {
+      inputLabel[i].classList.add('isfocu')
+      console.log(i)
+   })
+
+   element.addEventListener('blur', (e) => {
+      if(e.target.value === '') {
+         inputLabel[i].classList.remove('isfocu')
+      }
+      console.log('onblur')
+   })
+});
 
